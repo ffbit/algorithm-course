262a0d94ecc0c5f01c7ee1809d3410fecd52a0ce
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2012-08-19 22:32:20 +0300

    A not connected transitive test is green.

--- src/test/java/com/ffbit/week1/quickfind/QuickFindTest.java
+++ src/test/java/com/ffbit/week1/quickfind/QuickFindTest.java
@@ -24,27 +24,35 @@ public class QuickFindTest {
 
         assertTrue(find.connected(element, element));
     }
-    
+
     @Test
     public void itShouldBeSymmetricConnected() throws Exception {
         find.union(1, 2);
-        
+
         assertTrue(find.connected(1, 2));
     }
-    
+
     @Test
     public void itShouldNotBeSymmetricConnected() throws Exception {
         find.union(1, 2);
-        
+
         assertFalse(find.connected(2, 3));
     }
-    
+
     @Test
     public void itShouldBeConnectedTransitive() throws Exception {
         find.union(1, 2);
         find.union(2, 3);
-        
+
         assertTrue(find.connected(1, 3));
+    }
+
+    @Test
+    public void itShouldBeNotConnectedTransitive() throws Exception {
+        find.union(1, 2);
+        find.union(2, 3);
+
+        assertFalse(find.connected(0, 1));
     }
 
 }
